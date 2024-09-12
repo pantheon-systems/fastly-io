5e369244a4066976c526ecfaec7df0e97a5f37bb
Ryan Hoover <ryan.hoover@pantheon.io>
2024-09-12 11:15:13 -0500

    Add test that thumbnail not created, simplify setup

--- README.md
+++ README.md
@@ -14,10 +14,10 @@ wp media regenerate --yes
 
 ## Running tests
 
-Install MySQL and SVN if you don't already have it
+Install MariaDB and SVN if you don't already have it
 
 ```bash
-brew install mysql svn
+brew install mariadb svn
 ```
 
 Make sure you're running PHP with mysql, gd, gd-jpeg, and gd-webp support. If you're using phpbrew, a command like below will install the right package of PHP.
@@ -26,16 +26,16 @@ Make sure you're running PHP with mysql, gd, gd-jpeg, and gd-webp support. If yo
 phpbrew install 8.2.20 +default +fpm +pdo +mysql +gd +intl +openssl -- --with-jpeg --with-webp
 ```
 
-Initialize test environment
-
-```bash
-bin/install-wp-tests.sh wordpress_test root '' localhost latest
-```
-
 Install PHP Unit
 
 ```bash
 composer install
+```
+
+Initialize test environment
+
+```bash
+composer test-init
 ```
 
 Run the tests
--- tests/test-image-editor.php
+++ tests/test-image-editor.php
@@ -19,22 +19,31 @@ class Image_Editor_Test extends WP_UnitTestCase {
 	 * A single example test.
 	 */
 	public function test_get_image_editor() {
-		$editor = wp_get_image_editor( $this->jpg );
+		$editor = \wp_get_image_editor( $this->jpg );
 
 		$this->assertInstanceOf( 'Fastly_IO\WP_Image_Editor_Fastly', $editor );
 	}
 
 	public function test_make_subsizes() {
-		$attachment_id = $this->factory->attachment->create_object( [ 'file' => $this->jpg ] );
+		$attachment_id = $this->factory->attachment->create_upload_object( $this->jpg );
 
-		$meta = wp_create_image_subsizes( $this->jpg, $attachment_id );
+		$meta       = \wp_get_attachment_metadata( $attachment_id );
+		$upload_dir = \wp_upload_dir();
+		$sizes      = \wp_get_registered_image_subsizes();
 
-		$sizes = wp_get_registered_image_subsizes();
+		$file_path = \trailingslashit( $upload_dir['basedir'] ) . $meta['file'];
+
+		// Ensure the image was uploaded.
+		$this->assertFileExists( $file_path, 'The image was not uploaded' );
 
 		foreach ( $sizes as $size => $size_data ) {
 			if ( isset( $meta['sizes'][ $size ] ) ) {
+				// Ensure the thumbnail image did not get generated.
+				$file_subpath = str_replace( '.jpg', "-{$size_data['width']}x{$size_data['height']}.jpg", $file_path );
+				$this->assertFileDoesNotExist( $file_subpath, 'The thumbnail was locally created' );
+
 				$query_str = parse_url( $meta['sizes'][ $size ]['file'], PHP_URL_QUERY );
-				$params = wp_parse_args( $query_str );
+				$params = \wp_parse_args( $query_str );
 
 				// If width is non-zero, test they're equal.
 				if ( $size_data['width'] ) {
